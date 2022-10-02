ee21d6d1544f27cd33bcca80019fc375d2e4e5d5
Dušan Juretić <dusan.juretic@gmail.com>
2022-10-01 22:19:26 -0300

    Build to public folder

--- optimize.sh
+++ optimize.sh
@@ -3,7 +3,7 @@
 set -e
 
 js="app.js"
-min="app.min.js"
+min="public/app.min.js"
 
 elm make --optimize --output=$js "$@"
 
@@ -12,3 +12,10 @@ uglifyjs $js --compress 'pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A
 echo "Compiled size: $(wc $js -c) bytes  ($js)"
 echo "Minified size: $(wc $min -c) bytes  ($min)"
 echo "Gzipped size:  $(gzip $min -c | wc -c) bytes"
+
+npm run css-build:prod
+
+mkdir -p public/img
+cp -r img/128px_v2 public/img/
+
+sed 's#"../css/app.css#"app.min.css#; s/app.js/app.min.js/' index.html > public/index.html
